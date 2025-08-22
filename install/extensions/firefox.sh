a6f8cae083d50f0c44d4b86e69446678203ea73e
Stefan Luthardt <stefan.luthardt@outlook.com>
2025-08-22 11:19:03 +0800

    Update extensions

--- install/extensions/firefox.sh
+++ install/extensions/firefox.sh
@@ -11,9 +11,9 @@ mkdir $HOME/.firefox
 cd $HOME/.firefox
 
 curl https://addons.mozilla.org/firefox/downloads/file/4424459/reddit_enhancement_suite-5.24.8.xpi -o reddit_enhancement_suite.xpi
-curl https://addons.mozilla.org/firefox/downloads/file/4440363/bitwarden_password_manager-2025.2.0.xpi -o bitwarden_password_manager.xpi
-curl https://addons.mozilla.org/firefox/downloads/file/4412673/ublock_origin-1.62.0.xpi -o ublock_origin.xpi
-curl https://addons.mozilla.org/firefox/downloads/file/4439735/darkreader-4.9.103.xpi -o darkreader.xpi
+curl https://addons.mozilla.org/firefox/downloads/file/4552124/bitwarden_password_manager-2025.7.1.xpi -o bitwarden_password_manager.xpi
+curl https://addons.mozilla.org/firefox/downloads/file/4531307/ublock_origin-1.65.0.xpi -o ublock_origin.xpi
+curl https://addons.mozilla.org/firefox/downloads/file/4535824/darkreader-4.9.110.xpi -o darkreader.xpi
 
 firefox \
     bitwarden_password_manager.xpi \
